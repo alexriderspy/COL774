785de650f0b8be78aee2ed31a74fef81bb03c243
Sreemanti Dey <77806432+alexriderspy@users.noreply.github.com>
2022-10-25 07:38:27 +0530

    Update qa.py

--- ass3/Q1/Q1.2/qa.py
+++ ass3/Q1/Q1.2/qa.py
@@ -5,16 +5,15 @@ from sklearn import tree
 from sklearn.feature_extraction.text import CountVectorizer
 from scipy.sparse import hstack
 
-train_path = sys.argv[1]
-val_path = sys.argv[2]
-test_path = sys.argv[3]
+train_path = '.'
+val_path = '.'
+test_path = '.'
 
 train_data = pd.read_csv(train_path+'/DrugsComTrain.csv')
 
 y_train = train_data.rating
 
 train_data.drop('rating', inplace=True, axis=1)
-train_data.drop('usefulCount', inplace=True, axis=1)
 
 vectorizer1 = CountVectorizer()
 X_train_condition = vectorizer1.fit_transform(train_data.condition.astype('U'))
@@ -22,8 +21,10 @@ vectorizer2 = CountVectorizer()
 X_train_review = vectorizer2.fit_transform(train_data.review)
 vectorizer3 = CountVectorizer()
 X_train_date = vectorizer3.fit_transform(train_data.date)
+vectorizer4 = CountVectorizer()
+X_train_usefulCount = vectorizer4.fit_transform(train_data.usefulCount.astype('U'))
 
-X_train = hstack([X_train_condition, X_train_review, X_train_date])
+X_train = hstack([X_train_condition, X_train_review, X_train_date,X_train_usefulCount])
 # X_test = vectorizer.transform(X_test)
 
 dt = tree.DecisionTreeClassifier()
@@ -39,13 +40,13 @@ val_data = pd.read_csv(val_path + '/DrugsComVal.csv')
 y_val = val_data.rating
 
 val_data.drop('rating', inplace=True, axis=1)
-val_data.drop('usefulCount', inplace=True, axis=1)
 
 X_val_condition = vectorizer1.transform(val_data.condition.astype('U'))
 X_val_review = vectorizer2.transform(val_data.review)
 X_val_date = vectorizer3.transform(val_data.date)
+X_val_usefulCount = vectorizer4.transform(val_data.usefulCount.astype('U'))
 
-X_val = hstack([X_val_condition, X_val_review, X_val_date])
+X_val = hstack([X_val_condition, X_val_review, X_val_date,X_val_usefulCount])
 
 y_val_pred = dt.predict(X_val)
 val_acc = np.sum(y_val_pred == y_val)/len(y_val)
@@ -56,13 +57,13 @@ test_data = pd.read_csv(val_path + '/DrugsComTest.csv')
 y_test = test_data.rating
 
 test_data.drop('rating', inplace=True, axis=1)
-test_data.drop('usefulCount', inplace=True, axis=1)
 
 X_test_condition = vectorizer1.transform(test_data.condition.astype('U'))
 X_test_review = vectorizer2.transform(test_data.review)
 X_test_date = vectorizer3.transform(test_data.date)
+X_test_usefulCount = vectorizer4.transform(test_data.usefulCount.astype('U'))
 
-X_test = hstack([X_test_condition, X_test_review, X_test_date])
+X_test = hstack([X_test_condition, X_test_review, X_test_date,X_test_usefulCount])
 
 y_test_pred = dt.predict(X_test)
 test_acc = np.sum(y_test_pred == y_test)/len(y_test)
